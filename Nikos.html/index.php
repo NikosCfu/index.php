fb1ce8fb043ca7f8801dccf5306a29ad519d59cc
Nikos Michalakis <nikos.michalakis@hotmail.nl>
2021-05-10 13:45:20 +0200

    first commit

---
+++ Nikos.html/index.php
@@ -0,0 +1,36 @@
+
+    <?php
+    $aanhef ="Dhr";
+
+    $Voornaam ="Nikos";
+
+    $Achternaam = "Michalakis";
+
+    $straat = "Lindelaan";
+
+    $postcode = "2951 BN";
+    $woonplaats ="Alblasserdam";
+    $Land  ="Nederland";
+
+
+
+        echo $aanhef ;
+        echo " <br>" ;
+        echo $Voornaam ;
+        echo " <br>" ;
+        echo $Achternaam ;
+        echo " <br>" ;
+
+        echo $straat ;
+        echo " ";
+        echo $postcode ;
+        echo " ";
+        echo $woonplaats ;
+        echo " ";
+        echo  $Land ;
+
+
+
+
+        ?>
+    
---
+++ les1.1_oefening3.html/les1.1-oefening3.php
@@ -0,0 +1,35 @@
+<?php
+    $aanhef ="Dhr";
+
+    $Voornaam ="Nikos";
+
+    $Achternaam = "Michalakis";
+
+    $straat = "Lindelaan";
+
+    $postcode = "2951 BN";
+    $woonplaats ="Alblasserdam";
+    $Land  ="Nederland";
+
+
+
+        echo $aanhef ;
+        echo " " ;
+        echo $Voornaam ;
+        echo "" ;
+        echo $Achternaam ;
+        echo "<br> " ;
+
+        echo $straat ;
+        echo " ";
+        echo $postcode ;
+        echo " <br>";
+        echo $woonplaats ;
+        echo " <br>";
+        echo  $Land ;
+
+
+
+
+        ?>
+    
